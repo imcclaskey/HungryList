1130451d7593ff506e5a6ef9ece500e80c6bab40
Ian McClaskey <imcclaskey@gmail.com>
2015-12-02 12:59:05 -0500

    Add custom scrollbar build out buy list animations tweak responsiveness

--- client/templates/shop/shop.js
+++ client/templates/shop/shop.js
@@ -1,11 +1,15 @@
 Template.shop.onRendered(function() {
 
+	$('.buy-list').mCustomScrollbar({
+		theme:"inset-3-dark",
+		scrollInertia: 0,
+		scrollButtons: { enable: false }
+	});
+
+// item buttons expand
   $(".btn-buy").mouseover(function () {  
-
     $(this).find('.extend').stop().slideDown(100);
-
   }).mouseleave(function(){
-
     $(this).find('.extend').stop().slideUp(100);
   
   })    
