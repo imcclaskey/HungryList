3d24b2e8c2014c658d06b61a174c2344f5220f9b
Ian McClaskey <imcclaskey@gmail.com>
2015-12-02 21:59:16 -0500

    Buy-list tweaking

--- client/templates/shop/shop.js
+++ client/templates/shop/shop.js
@@ -1,10 +1,30 @@
 Template.shop.onRendered(function() {
 
+
+	// scrollbar 
+	var listHeight = $('.buy-list')[0].scrollHeight;
+	console.log(listHeight);
+	var offset = listHeight % 85;
+	console.log(offset);
+
+	//$('.buy-list').height(function (index, height) {
+	//	
+  //  return (height + 32);
+	//});
+
 	$('.buy-list').mCustomScrollbar({
 		theme:"inset-3-dark",
 		scrollInertia: 0,
-		scrollButtons: { enable: false }
+		scrollButtons: { enable: false },
+		mouseWheel:{ scrollAmount: 85 },
+		callbacks:{
+      onTotalScroll: function(offset){
+      }
+		}
 	});
+
+
+
 
 // item buttons expand
   $(".btn-buy").mouseover(function () {  
